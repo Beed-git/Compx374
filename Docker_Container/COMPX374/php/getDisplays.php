4f97c8d9f43c9f9036903b43f06162ad602bc144
Samantha Steed <63430011+samsteed@users.noreply.github.com>
2022-10-25 12:08:59 +1300

    Fixed Edit Display page layout

--- Docker_Container/COMPX374/php/getDisplays.php
+++ Docker_Container/COMPX374/php/getDisplays.php
@@ -19,102 +19,76 @@
 	//Check that we get data back
 	if($result1 != FALSE)
 	{
-		//Output the header
+    //Output the header
 		echo "<h1>Edit Displays</h1>";
 		
-		//Initialize the counter for the competitions
-		$countCompetition = 0;
-		
-		//Repeat for each competition
-		while($rowCompetition = $result1->fetch()) 
-		{
-			echo "<div class='competition-container'>";
-      
-      //Output the competition name
-      echo "<h2>Competition Name: ".$rowCompetition['name']."</h2>";
-      
-      //Get all displays in that competition
-      $query = "select * from Display where competition_id=".$rowCompetition['competition_id'];
-    	$result2 = $con->query($query);
-			
-			if($result2 != FALSE)
-			{
-				//Initialise the counter for the displays in this competiton
-        $countDisplays = 0;
-        
-        echo "<div class='competition-displays-container'>";
-        echo "<p>Before the displays</p>";
-        
-		    //Repeat for each competition
-		    while($rowDisplay = $result2->fetch()) 
-		    {
-          //Get the media associated with this display
-          $query = "select * from Media where media_id = (select media_id from Media_Instance where minstance_id = (select minstance_id from Display_Contains where display_id=".$rowDisplay['display_id']."))";
-        	$result3 = $con->query($query);
+    //Get all of the displays
+    $query = "select * from Display";
+    $result1 = $con->query($query);
+   
+    //Check that we get the data back
+    if($result1 != FALSE)
+    {
+      //Initialise the counter for the displays in this competiton
+      $countDisplays = 0;
+     
+      //Repeat for each display
+      while($rowDisplay = $result1->fetch()) 
+      {
+        //Get the media associated with this display
+        $query = "select * from Media where media_id = (select media_id from Media_Instance where minstance_id = (select minstance_id from Display_Contains where display_id=".$rowDisplay['display_id']."))";
+        $result2 = $con->query($query);
           
+        //Check that we get the data back
+        if($result2 != FALSE)
+        {
+          $rowMedia = $result2->fetch();
+            
+          //Get the artist informations associated with this media
+          $query = "select * from Artist where artist_id = ".$rowMedia['artist_id'];
+          $result3 = $con->query($query);
+            
           //Check that we get the data back
           if($result3 != FALSE)
-		      {
-            $rowMedia = $result3->fetch();
-            
-            //Get the artist informations associated with this media
-            $query = "select * from Artist where artist_id = ".$rowMedia['artist_id'];
-            $result4 = $con->query($query);
-            
-            //Check that we get the data back
-            if($result4 != FALSE)
-		        {
-              $rowArtist = $result4->fetch();
+          {
+            $rowArtist = $result3->fetch();
               
-              //Output the display information
-              echo "<div class='submission selectable' onclick='viewSubmission(".$rowMedia['media_id'].");'>";
-					    echo "<div class='image-container'>";
-					    echo "<img src='".$rowMedia['media_url']."' alt='Media Submission Image'>";
-			        echo "</div>";
-              echo "<div class='submission-description'>";
-					    echo "<p>Mural Name: ".$rowMedia['name']."</p>";
-					    echo "<p>Artist Name: ".$rowArtist['username']."</p>";
-              echo "</div>";
-					    echo "</div>";
-            }
+            //Output the display information
+            echo "<div class='submission' onclick='viewSubmission(".$rowMedia['media_id'].");'>";
+            echo "<div class='image-container'>";
+            echo "<img src='".$rowMedia['media_url']."' alt='Media Submission Image'>";
+            echo "</div>";
+            echo "<div class='submission-description'>";
+            echo "<p>Mural Name: ".$rowMedia['name']."</p>";
+            echo "<p>Artist Name: ".$rowArtist['username']."</p>";
+            echo "</div>";
+            echo "<button class='approve-button' id='editSubmission' type=submit' onclick='editSubmission(".$rowMedia['media_id'].")'>Edit</button>";
+            echo "<button class='disapprove-button' id='deleteSubmission' type=submit' onclick='deleteDisplay(".$rowMedia['media_id'].")'>Delete</button>";
+            echo "</div>";
           }
           else
-	        {
-		        die("Error in database query");
-	        }
-          
-          //Increment the counter
-		      $countDisplays = $countDisplays + 1;
+          {
+            die("Error in database query!");
+          }
         }
-        
-		    //Check if there are no displays yet
-		    if ($countDisplays == 0)
-		    {
-          echo "<p>There are no displays for this competition.</p>";
-		    }
-            
-        echo "<p>After the displays</p>";
-        echo "</div>";
-			}
-			else
-			{
-				die("Error in database query");
-			}
       
-			//Increment the counter
-			$countCompetition = $countCompetition + 1;
+        //Increment the counter
+        $countDisplays = $countDisplays + 1;
+      }
       
-      echo "</div>";
-		}
-
-		//Check if there have been no competitions yet
-		if ($countCompetition == 0)
-		{
-			echo "<p>There are no competitions.</p>";
-		}		
-	}
-	else
-	{
-		die("Error in database query");
-	}
+      //Check if there are no displays yet
+      if ($countDisplays == 0)
+      {
+        echo "<p>There are no displays for this competition.</p>";
+      }
+    }
+    else
+    {
+      die("Error in database query");
+    }
+  }
+  else
+  {
+    die("Error in database query");
+  }
 ?>
