d5a4c72d8b8d25de235394e92664730927e56ddd
Samantha Steed <63430011+samsteed@users.noreply.github.com>
2022-10-28 11:22:55 +1300

    Added user type signin options

--- Docker_Container/COMPX374/index.php
+++ Docker_Container/COMPX374/index.php
@@ -10,8 +10,9 @@
 	{
 		//Retrieve the form data
 		$email = $_POST['email'];
-        $password = $_POST['password'];
-		
+    $password = $_POST['password'];
+    $user_type = $_POST['user_type'];
+		 
 		//Query the Artist and Moderator tables for the email
 		$artistQuery = "select * from Artist where email = '".$email."'";
 		$artistResult = $con->query($artistQuery);
@@ -26,12 +27,12 @@
 			$moderatorRow = $moderatorResult->fetch();
 			
 			//Check that the email belongs to an artist
-			if (gettype($artistRow) == 'array')
-			{
-				//Check that the user has entered the correct password
+      if ($user_type == 'option_user' AND gettype($artistRow) == 'array')
+			{        
+        //Check that the user has entered the correct password
 				if (password_verify($password, $artistRow['password']))
-				{
-					$_SESSION["loggedin"] = true;
+				{          
+          $_SESSION["loggedin"] = true;
 					$_SESSION['email'] = $email;
 					header("Location: php/upload.php");
 				}
@@ -42,10 +43,12 @@
 				}
 			}
 			//Check if the email belongs to a moderator
-			elseif (gettype($moderatorRow) == 'array')
+			elseif ($user_type == 'option_moderator' AND gettype($moderatorRow) == 'array')
 			{
-				//Check that the user has entered the correct password
-				if (password_verify($password, $moderatorRow['password']))
+        //Check that the user has entered the correct password
+				
+        echo password_verify($password, $moderatorRow['password']);
+        if (password_verify($password, $moderatorRow['password']))
 				{
 					$_SESSION["loggedin"] = true;
 					$_SESSION['email'] = $email;
@@ -78,19 +81,26 @@
 		<link href="css/tuakiri.css" rel="stylesheet" type="text/css">
 	</head>
 	<body>		
-		<form action="" method="post" name="signin-form">
-			<h1>Log into Tuakiri</h1>
-			<div class="form-element">
-				<input type="email" id="email" name="email" placeholder="Email" required />
-			</div>
-			<div class="form-element">
-				<input type="password" id="password" name="password" placeholder="Password" required />
-			</div>
-			<div>
-				<a href="php/forgot-password.php">Forgot password?</a>
-			</div>
-			<button class="form-button" type="submit" name="login" value="login">Log In</button>
-			<p>No account? <a href="php/register.php">Create one</a></p>
-		</form>
+		  <br><br>
+      <form action="" method="post" name="signin-form">
+			  <h1>Log into Tuakiri</h1>
+			  <div class="form-element">
+				  <input type="email" id="email" name="email" placeholder="Email" required />
+			  </div>
+			  <div class="form-element">
+				  <input type="password" id="password" name="password" placeholder="Password" required />
+			  </div>
+        <div class="form-element radio-button">
+          <label for"option_user">User</label>
+          <input type="radio" class="radio" id="option_user" name="user_type" value="option_user" checked>
+          <label for"option_moderator">Moderator</label>
+          <input type="radio" id="option_moderator" name="user_type" value="option_moderator">
+			  </div>
+			  <div>
+				  <a href="php/forgot-password.php">Forgot password?</a>
+			  </div>
+			  <button class="form-button" type="submit" name="login" value="login">Log In</button>
+			  <p>No account? <a href="php/register.php">Create one</a></p>
+		  </form>
 	</body>
 </html>	
