84b813fe522da08c5aa6c76c0513726427d521cb
Samantha Steed <63430011+samsteed@users.noreply.github.com>
2022-10-28 21:12:11 +1300

    Fixed the edit displays page

--- Docker_Container/COMPX374/php/editDisplays.php
+++ Docker_Container/COMPX374/php/editDisplays.php
@@ -1,94 +1,47 @@
 <?php
 	// Initialize the session
 	session_start();
-	
-	//Connect to the database
-	require_once('connection.php');
  
-	// Check if the user is logged in, if not then redirect them to login page
+	// Check if the user is logged in, if not then redirect him to login page
 	if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true)
 	{
 		header("location: ../index.php");
 		exit;
 	}
- 
-  //Get all competition IDs in descending order
-  $query = "select * from Competition order by competition_id DESC";
-	$result1 = $con->query($query);
-	
-	//Check that we get data back
-	if($result1 != FALSE)
-	{
-    //Output the header
-		echo "<h1>Edit Displays</h1>";
-		
-    //Get all of the displays
-    $query = "select * from Display";
-    $result1 = $con->query($query);
-   
-    //Check that we get the data back
-    if($result1 != FALSE)
-    {
-      //Initialise the counter for the displays in this competiton
-      $countDisplays = 0;
-     
-      //Repeat for each display
-      while($rowDisplay = $result1->fetch()) 
-      {
-        //Get the media associated with this display
-        $query = "select * from Media where media_id = (select media_id from Media_Instance where minstance_id = (select minstance_id from Display_Contains where display_id=".$rowDisplay['display_id']."))";
-        $result2 = $con->query($query);
-          
-        //Check that we get the data back
-        if($result2 != FALSE)
-        {
-          $rowMedia = $result2->fetch();
-            
-          //Get the artist informations associated with this media
-          $query = "select * from Artist where artist_id = ".$rowMedia['artist_id'];
-          $result3 = $con->query($query);
-            
-          //Check that we get the data back
-          if($result3 != FALSE)
-          {
-            $rowArtist = $result3->fetch();
-              
-            //Output the display information
-            echo "<div class='submission' onclick='viewSubmission(".$rowMedia['media_id'].");'>";
-            echo "<div class='image-container'>";
-            echo "<img src='".$rowMedia['media_url']."' alt='Media Submission Image'>";
-            echo "</div>";
-            echo "<div class='submission-description'>";
-            echo "<p>Mural Name: ".$rowMedia['name']."</p>";
-            echo "<p>Artist Name: ".$rowArtist['username']."</p>";
-            echo "</div>";
-            echo "<button class='approve-button' id='editDisplay' type=submit' onclick='editDisplay(".$rowDisplay['display_id'].")'>Edit</button>";
-            echo "<button class='disapprove-button' id='deleteDisplay' type=submit' onclick='deleteDisplay(".$rowDisplay['display_id'].")'>Delete</button>";
-            echo "</div>";
-          }
-          else
-          {
-            die("Error in database query!");
-          }
-        }
-      
-        //Increment the counter
-        $countDisplays = $countDisplays + 1;
-      }
-      
-      //Check if there are no displays yet
-      if ($countDisplays == 0)
-      {
-        echo "<p>There are no displays for this competition.</p>";
-      }
-    }
-    else
-    {
-      die("Error in database query");
-    }
-  }
-  else
-  {
-    die("Error in database query");
-  }
 ?>
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<title>Edit Displays</title>
+		<link href="../css/tuakiri.css" rel="stylesheet" type="text/css">
+		<script>
+			//Fetch the submissions for the current competition
+			let getDisplays = () => {
+				fetch("getDisplays.php", {method: 'post'}).then(response => response.text()).then(displayResults);
+			}
+      
+			//Delete the display
+			let deleteDisplay = (display_id) => {
+				if (confirm("Are you sure you want to permanently delete this display?")) {
+					fetch("deleteDisplay.php", {method: 'post', body: display_id}).then(response => response.text()).then(displayResults);
+				}
+			}
+			
+			//Display the results
+			let displayResults = (response) => {
+				document.getElementById("displays").innerHTML = response;
+			}
+		</script>
+	</head>
+	<body onload="getDisplays();">
+		<div class="topnav">
+			<a href="submissions.php">Review Submissions</a>
+			<a class="active" href="editDisplays.php">Edit Displays</a>
+			<a href="newCompetition.php">New Competition</a>
+			<a href="newModerator.php">Register New Moderator</a>
+			<a class="logout" href="logout.php">Log out</a>
+		</div>
+		<div id="displays"></div>
+	</body>
+</html>	
