43e748bf2e112fda8cb516cc8f3f3ceae7a23867
Brad Podmore <bp66@students.waikato.ac.nz>
2022-11-09 15:37:36 +1300

    stuff

--- Docker_Container/WebAPI/app/controllers/media.controller.js
+++ Docker_Container/WebAPI/app/controllers/media.controller.js
@@ -3,6 +3,8 @@ const Media = require("../models/media.model");
 const getMediaQuery = async function(req, res) {
     if (req.query.artist_id) {
         await getMediaByArtist(req, res, req.query.artist_id);
+    } else if (req.query.display_id) {
+        await getMediaByDisplay(req, res, req.query.display_id);
     } else {
         getMedia(req, res);
     }
@@ -48,6 +50,18 @@ const getMediaByArtist = async function (req, res, artist_id) {
             res.sendStatus(500);
         }
     }
+}
+
+const getMediaByDisplay = async function (req, res, display_id) {
+    if (!isNaN(req.params.id)) {
+        try {
+            const media = await Media.getMediaByDisplay(display_id);
+            res.status(200).json({media: media});
+        } catch (err) {
+            console.log(err);
+            res.sendStatus(500);
+        }
+    })
 }
 
 module.exports = {
